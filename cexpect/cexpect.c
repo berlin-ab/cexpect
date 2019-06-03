af306c7c1c75482a078556afe65d87bb32b19abf
Adam Berlin <aberlin@pivotal.io>
2019-06-03 17:24:45 -0400

    Organize with comments.

--- cexpect/cexpect.c
+++ cexpect/cexpect.c
@@ -42,12 +42,47 @@ struct FormatterData {
     void (*report_start)(Suite *suite);
 };
 
+/*
+ * Suite
+ */
+Suite *make_suite(char *suite_name) {
+	Suite *suite = calloc(1, sizeof(Suite));
+	suite->formatter = make_dot_formatter();
+	suite->name = suite_name;
+
+	for (int i = 0; i < MAX_SUITE_SIZE; i++) {
+		Test test;
+		test.is_active = false;
+		suite->tests[i] = test;
+	}
+
+	return suite;
+}
+
 
 char *get_suite_name(Suite *suite) {
 	return suite->name;
 }
 
 
+int number_of_tests(Suite *suite) {
+	return suite->size;
+}
+
+
+int number_of_failed_tests(Suite *suite) {
+	return suite->number_of_failed_tests;
+}
+
+
+int number_of_passing_tests(Suite *suite) {
+	return suite->number_of_passing_tests;
+}
+
+
+/* 
+ * Failed tests
+ */
 FailedTest *get_failed_test(Suite *suite, int test_number) {
 	return &suite->failed_tests[test_number];
 }
@@ -72,20 +107,21 @@ char *get_failing_test_file_name(FailedTest *failed_test) {
 	return failed_test->file_name;
 }
 
+/*
+ * Tests
+ */
+void add_test_to_suite(Suite *suite, void (*test_function)(Test *test), int line_number, char *file_name) {
+	Test test;
+	test.is_active = true;
+	test.test_function = test_function;
+	test.line_number = line_number;
+	test.file_name = file_name;
+	test.suite = suite;
 
-int number_of_tests(Suite *suite) {
-	return suite->size;
+	suite->tests[suite->size] = test;
+	suite->size++;
 }
 
-
-int number_of_failed_tests(Suite *suite) {
-	return suite->number_of_failed_tests;
-}
-
-
-int number_of_passing_tests(Suite *suite) {
-	return suite->number_of_passing_tests;
-}
 
 void pass_test(Test *test) {
 	test->suite->number_of_passing_tests++;
@@ -106,6 +142,9 @@ void fail_test(Test *test, char *expected_value, char *actual_value) {
 }
 
 
+/*
+ * Assertions
+ */
 void expect_equal(Test *test, int expected_value, int actual_value) {
     if (expected_value == actual_value) {
 		pass_test(test);
@@ -121,6 +160,9 @@ void expect_equal(Test *test, int expected_value, int actual_value) {
 }
 
 
+/* 
+ * Void formatter 
+ */
 void report_failing_test_with_void(Test *test) {
 }
 
@@ -138,15 +180,16 @@ void report_start_for_void(Suite *suite) {
 
 
 Formatter *make_void_formatter() {
-    Formatter *formatter = calloc(1, sizeof(Formatter));
-    formatter->fail = report_failing_test_with_void;
-    formatter->success = report_successful_test_with_void;
-    formatter->summary = report_summary_for_void;
-    formatter->report_start = report_start_for_void;
-    return formatter;
+	return make_formatter(
+		report_failing_test_with_void,
+		report_successful_test_with_void,
+		report_summary_for_void,
+		report_start_for_void);
 }
 
-
+/*
+ * Formatter extension point
+ */
 Formatter *make_formatter(
 	void (*fail)(Test *test),
 	void (*success)(Test *test),
@@ -162,24 +205,14 @@ Formatter *make_formatter(
 }
 
 
-void add_test_to_suite(Suite *suite, void (*test_function)(Test *test), int line_number, char *file_name) {
-    Test test;
-    test.is_active = true;
-    test.test_function = test_function;
-    test.line_number = line_number;
-    test.file_name = file_name;
-    test.suite = suite;
-    
-    suite->tests[suite->size] = test;
-    suite->size++;
-}
-
-
 void set_formatter(Suite *suite, Formatter *formatter) {
     suite->formatter = formatter;
 }
 
 
+/*
+ * Runner
+ */
 int run_suite(Suite *suite) {
     suite->formatter->report_start(suite);
 
@@ -190,21 +223,6 @@ int run_suite(Suite *suite) {
     suite->formatter->summary(suite);
 
     return suite->number_of_failed_tests > 0;
-}
-
-
-Suite *make_suite(char *suite_name) {
-    Suite *suite = calloc(1, sizeof(Suite));
-    suite->formatter = make_dot_formatter();
-    suite->name = suite_name;
-
-    for (int i = 0; i < MAX_SUITE_SIZE; i++) {
-        Test test;
-	test.is_active = false;
-	suite->tests[i] = test;
-    }
-
-    return suite;
 }
 
 
