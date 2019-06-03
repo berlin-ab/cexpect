27052ea96938800e9faeea20c2eb31a17c2ae618
Adam Berlin <aberlin@pivotal.io>
2019-06-03 11:32:56 -0400

    cleanup.

--- test/examples/list_test.c
+++ test/examples/list_test.c
@@ -6,6 +6,7 @@
 
 #include "list.h"
 
+
 /*
  * list matcher comparisons
  */
@@ -66,10 +67,13 @@ Matcher *list_has_size(void *size) {
 }
 
 
+/* 
+ * Tests
+ */
 void a_list_starts_as_empty(Test *test) {
-    List *list = make_list();
+	List *list = make_list();
 
-    expect(test, list, list_is_empty());
+	expect(test, list, list_is_empty());
 }
 
 
@@ -96,9 +100,9 @@ void a_list_starts_with_size_zero(Test *test) {
 
 
 int main(int argc, char *args[]) {
-  Suite *suite = make_suite("List test");
-  add_test(suite, a_list_starts_as_empty);
-  add_test(suite, a_list_starts_with_size_zero);
-  add_test(suite, a_list_is_no_longer_empty_after_adding_to_list);
-  start_cexpect(suite);
+	Suite *suite = make_suite("List test");
+	add_test(suite, a_list_starts_as_empty);
+	add_test(suite, a_list_starts_with_size_zero);
+	add_test(suite, a_list_is_no_longer_empty_after_adding_to_list);
+	start_cexpect(suite);
 }
