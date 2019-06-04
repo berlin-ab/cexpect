2a8ddd487a66ec3d15896940015d2e0c42fa15ba
Adam Berlin <aberlin@pivotal.io>
2019-06-03 23:44:42 -0400

    Replace array implementations with list.

--- cexpect/cexpect.c
+++ cexpect/cexpect.c
@@ -7,11 +7,7 @@
 #include "cexpect_dot_formatter.h"
 
 
-#define MAX_SUITE_SIZE 100
-
-
 struct TestData {
-	bool is_active;
 	void (*test_function)(Test *test);
 	Suite *suite;
 	int line_number;
@@ -30,9 +26,7 @@ struct FailedTestData {
 struct SuiteData {
 	char *name;
 	List *tests;
-	FailedTest failed_tests[MAX_SUITE_SIZE];
-	int size;
-	int number_of_failed_tests;
+	List *failed_tests;
 	int number_of_passing_tests;
 	Formatter *formatter;
 };
@@ -54,6 +48,7 @@ Suite *make_suite(char *suite_name) {
 	suite->formatter = make_dot_formatter();
 	suite->name = suite_name;
 	suite->tests = make_list();
+	suite->failed_tests = make_list();
 
 	return suite;
 }
@@ -70,12 +65,12 @@ char *get_suite_name(Suite *suite) {
 
 
 int number_of_tests(Suite *suite) {
-	return suite->size;
+	return list_size(suite->tests);
 }
 
 
 int number_of_failed_tests(Suite *suite) {
-	return suite->number_of_failed_tests;
+	return list_size(suite->failed_tests);
 }
 
 
@@ -88,7 +83,16 @@ int number_of_passing_tests(Suite *suite) {
  * Failed tests
  */
 FailedTest *get_failed_test(Suite *suite, int test_number) {
-	return &suite->failed_tests[test_number];
+	int index = 0;
+	
+	for (ListItem *item = list_first(suite->failed_tests); item; item = list_next(item)) {
+		if (test_number == index) {
+			return (FailedTest *)list_value(item);
+		}
+		index++;
+	}
+	
+	return NULL;
 }
 
 
@@ -116,15 +120,12 @@ char *get_failing_test_file_name(FailedTest *failed_test) {
  */
 void add_test_to_suite(Suite *suite, void (*test_function)(Test *test), int line_number, char *file_name) {
 	Test *test = calloc(1, sizeof(Test));
-	test->is_active = true;
 	test->test_function = test_function;
 	test->line_number = line_number;
 	test->file_name = file_name;
 	test->suite = suite;
-	
+
 	add_to_list(suite->tests, test);
-	
-	suite->size++;
 }
 
 
@@ -135,14 +136,13 @@ void pass_test(Test *test) {
 
 
 void fail_test(Test *test, char *expected_value, char *actual_value) {
-	FailedTest failed_test;
-	failed_test.expected_value = expected_value;
-	failed_test.actual_value = actual_value;
-	failed_test.line_number = test->line_number;
-	failed_test.file_name = test->file_name;
+	FailedTest *failed_test = calloc(1, sizeof(FailedTest));
+	failed_test->expected_value = expected_value;
+	failed_test->actual_value = actual_value;
+	failed_test->line_number = test->line_number;
+	failed_test->file_name = test->file_name;
 
-	test->suite->failed_tests[test->suite->number_of_failed_tests] = failed_test;
-	test->suite->number_of_failed_tests++;
+	add_to_list(test->suite->failed_tests, failed_test);
 	test->suite->formatter->fail(test);
 }
 
@@ -196,7 +196,7 @@ int run_suite(Suite *suite) {
 
 	suite->formatter->summary(suite);
 
-	return suite->number_of_failed_tests > 0;
+	return number_of_failed_tests(suite) > 0;
 }
 
 
