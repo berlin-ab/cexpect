7d309597b5af5c08771f4fd0271026a5c1fa325c
Adam Berlin <aberlin@pivotal.io>
2019-06-03 23:55:37 -0400

    Clean.

--- cexpect_list/cexpect_list.h
+++ cexpect_list/cexpect_list.h
@@ -6,9 +6,12 @@
 typedef struct ListItemData ListItem;
 typedef struct ListData List;
 
+// List
 List *make_list(void);
 void add_to_list(List *list, void *value);
 int list_size(List *list);
+
+// List items
 ListItem *list_next(ListItem *item);
 ListItem *list_first(List *list);
 void *list_value(ListItem *item);
