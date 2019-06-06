8a7097511d11a998e2c4608efbb6b2f56fae6e24
Adam Berlin <aberlin@pivotal.io>
2019-06-06 10:45:54 -0400

    Remove the dot formatter's dependency on stdio.

--- cexpect_dot_formatter/dot_formatter.c
+++ cexpect_dot_formatter/dot_formatter.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-
-
 #include "cexpect.h"
 #include "cexpect_formatter.h"
 #include "cexpect_list.h"
@@ -8,21 +5,22 @@
 
 typedef int (*printer_function_type)(const char *format, ...);
 
-static printer_function_type printer = printf;
+
+static printer_function_type internal_printer;
 
 
 static void report_failing_test_with_dot() {
-	printer("F");
+	internal_printer("F");
 }
 
 
 static void report_successful_test_with_dot() {
-	printer(".");
+	internal_printer(".");
 }
 
 
 static void report_start_for_dots(char *suite_name) {
-	printf("Running suite: %s\n", suite_name);
+	internal_printer("Running suite: %s\n", suite_name);
 }
 
 
@@ -32,28 +30,28 @@ static void report_summary_for_dots(
 	int number_of_failed_tests,
 	FailedTest failed_tests[]
 	) {
-	printf("\n");
-	printf("\n");
-	printf("Summary:\n");
-	printf("\nRan %d test(s).\n\n%d passed, %d failed",
+	internal_printer("\n");
+	internal_printer("\n");
+	internal_printer("Summary:\n");
+	internal_printer("\nRan %d test(s).\n\n%d passed, %d failed",
 	       number_of_tests,
 	       number_of_passing_tests,
 	       number_of_failed_tests);
-	printf("\n\n");
+	internal_printer("\n\n");
 	for(int i = 0; i < number_of_failed_tests; i++) {
 		FailedTest failed_test = failed_tests[i];
-		printf("expected %s, got %s -- %s:%d\n",
+		internal_printer("expected %s, got %s -- %s:%d\n",
 		       failed_test.expected_value,
 		       failed_test.actual_value,
 		       failed_test.file_name,
 		       failed_test.line_number);
 	}
-	printf("\n");
+	internal_printer("\n");
 }
 
 
 Formatter *make_dot_formatter(printer_function_type new_printer) {
-	printer = new_printer;
+	internal_printer = new_printer;
 	
 	return make_formatter(
 		report_failing_test_with_dot,
