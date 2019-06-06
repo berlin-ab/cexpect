3cf062abd2c5a0f24178c67e9f10c7b7c5b2bb3f
Adam Berlin <aberlin@pivotal.io>
2019-06-06 13:34:24 -0400

    Make the formatters only depend on the formatter header.

--- cexpect_dot_formatter/cexpect_dot_formatter.h
+++ cexpect_dot_formatter/cexpect_dot_formatter.h
@@ -1,7 +1,6 @@
 #ifndef CEXPECT_DOT_FORMATTER_H
 #define CEXPECT_DOT_FORMATTER_H
 
-#include "cexpect.h"
 #include "cexpect_formatter.h"
 
 extern Formatter *make_dot_formatter();
--- cexpect_void_formatter/cexpect_void_formatter.h
+++ cexpect_void_formatter/cexpect_void_formatter.h
@@ -1,7 +1,7 @@
 #ifndef CEXPECT_VOID_FORMATTER_H
 #define CEXPECT_VOID_FORMATTER_H
 
-#include "cexpect.h"
+#include "cexpect_formatter.h"
 
 extern Formatter *make_void_formatter();
 
