c2bef21a4a9ab72ab36e8faecd4379b6ac66e24d
Adam Berlin <aberlin@pivotal.io>
2019-06-08 22:24:34 -0400

    ifndef for header.

--- cexpect/cexpect_suite_types.h
+++ cexpect/cexpect_suite_types.h
@@ -1,3 +1,8 @@
+#ifndef CEXPECT_SUITE_TYPES_H
+#define CEXPECT_SUITE_TYPES_H
+
 typedef struct SuiteData Suite;
 typedef struct TestData Test;
 typedef void (*test_function_type)(Test *test);
+
+#endif // CEXPECT_SUITE_TYPES_H
